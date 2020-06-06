9a4cb318bb630cb5757db02c23844fae2b18cc3e
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2020-06-06 22:51:34 +0900

    fix title

--- __tests__/userDefinedTypeGuard.test.ts
+++ __tests__/userDefinedTypeGuard.test.ts
@@ -5,7 +5,7 @@ describe("userDefinedTypeGuard.ts", () => {
   const srcFileName = `__tests__/userDefinedTypeGuard.ts`;
   const infer = createDeclarationInferencer(srcFileName);
 
-  test("trueth", () => {
+  test("Changes Inference by User Defined Type Guard", () => {
     expect(infer("res1")).toBe("(number | null | undefined)[]");
     expect(infer("res2")).toBe("(number | null)[]");
     expect(infer("res3")).toBe("number[]");
